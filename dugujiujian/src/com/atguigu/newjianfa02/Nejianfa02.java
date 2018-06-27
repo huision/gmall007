64f4873a2fbb1ee09d1588a305825b686f8f35f7
zhangchen <windyzj@163.com>
2018-06-27 11:24:47 +0800

    新剑法

---
+++ dugujiujian/src/com/atguigu/newjianfa02/Nejianfa02.java
@@ -0,0 +1,11 @@
+package com.atguigu.newjianfa02;
+
+/**
+ * @param
+ * @return
+ */
+public class Nejianfa02 {
+    public static void main(String[] args) {
+        System.out.println("hello newjianfa02");
+    }
+}
