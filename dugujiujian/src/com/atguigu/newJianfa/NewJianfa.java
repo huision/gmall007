5e97df3c349c5e20d17b7e567386e9f8aea9cebb
zhangchen <windyzj@163.com>
2018-06-27 10:46:48 +0800

    新剑法

---
+++ dugujiujian/src/com/atguigu/newJianfa/NewJianfa.java
@@ -0,0 +1,12 @@
+package com.atguigu.newJianfa;
+
+/**
+ * @param
+ * @return
+ */
+public class NewJianfa {
+
+    public static void main(String[] args) {
+        System.out.println("new 剑法");
+    }
+}
